3b0252b31b2c24707ee0326e7a8f42bbf7bb1f0a
unknown <weuller-santos@hotmail.com>
2020-05-18 22:44:20 -0300

    Ajuste

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next)=>{
 });
 
 app.use((error, req, res, next)=>{
-    res.status(erro.status || 500);
+    res.status(error.status || 500);
     res.json({
         error:{
             message:error.message
